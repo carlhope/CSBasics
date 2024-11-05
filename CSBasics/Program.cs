eab4888b3ccca964d619bacae467b5c294c68a27
Carl hope <84421131+carlhope@users.noreply.github.com>
2024-11-05 15:30:11 +0000

    completed task 6

--- CSBasics/Program.cs
+++ CSBasics/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double pi = 3.14;
+            bool isLearningCSharp = true;
+            string favouriteFilm = "Matrix";
+            favouriteFilm = "High School Musical";
+            printGreeting("Charlie");
+            Console.WriteLine(methodName(1, 2));
+
+        }
+        private static void printGreeting(string name)
+        {
+            Console.WriteLine("Hello, " + name);
+        }
+        private static int methodName(int parameterOne, int parameterTwo)
+        {
+            return parameterOne - parameterTwo;
         }
     }
 }
